07657f9438ee066b7ed84d282572cc42b0a6521b
Gaetan Brison <gaetan.brison@gmail.com>
2021-11-29 13:34:58 +0100

    Setup update

--- setup.py
+++ setup.py
@@ -1,1 +1,14 @@
-from setuptools
+# Import required functions
+from setuptools import setup, find_packages
+
+# Call setup function
+setup(
+    author="Florence d'Alché-Buc (Researcher), Luc Motte (Researcher), Awais Sani (Engineer), Amine Yamoul (Engineer), Gaëtan Brison (Engineer)",
+    description="Test package using IOKR method with the long term goal to develop a Structured-Prediction Package",
+    name="IOKR - (Input - Output Kernel Regression)",
+    version="0.1.0",
+    install_requires=["pandas","numpy","scipy","scikit-learn"],
+    python_requires=">=2.7, !=3.0.* !=3.1.*"
+)
+
+
