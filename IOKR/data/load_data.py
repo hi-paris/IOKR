21505b452b9b032a6044d4fde225b82b591a6138
Gaetan Brison <gaetan.brison@gmail.com>
2021-12-12 17:26:56 +0100

    load_data update and line profiler

--- IOKR/data/load_data.py
+++ IOKR/data/load_data.py
@@ -26,25 +26,34 @@ import random
 
 dir_path = os.path.dirname(os.path.realpath(__file__))
 
-
+@profile
 def load_bibtex(dir_path: str):
     """
     Load the bibtex dataset.
     __author__ = "Michael Gygli, ETH Zurich"
     from https://github.com/gyglim/dvn/blob/master/mlc_datasets/__init__.py
-    number of labels ("tags") = 159
-    dimension of inputs = 1836
+
+    Parameters
+    ----------
+    dir_path : string - containing location of bibtex.arff
+
+
     Returns
     -------
-    txt_labels (list)
-        the 159 tags, e.g. 'TAG_system', 'TAG_social_nets'
-    txt_inputs (list)
-        the 1836 attribute words, e.g. 'dependent', 'always'
-    labels (np.array)
-        N x 159 array in one hot vector format
-    inputs (np.array)
-        N x 1839 array in one hot vector format
+    X : np.array
+        Explanatory variables - N * 1836 array variables in one vector - e.g. 'dependent', 'always'
+
+    Y : np.array
+        Target variables - N * 159 array variables in one vector - e.g. 'TAG_system', 'TAG_social_nets'
+
+    X_txt : list
+            Explanatory variables - N * 1836 list variables in one vector - e.g. 'dependent', 'always'
+
+    Y_txt : list
+            Target variables - N * 159 list variables in one vector - e.g. 'TAG_system', 'TAG_social_nets'
+
     """
+
     feature_idx = 1836
 
     dataset = arff.load(open('%s/bibtex.arff' % dir_path), "r")
@@ -63,22 +72,19 @@ def load_bibtex(dir_path: str):
 path = "../data/bibtex/"
 X, Y, _, _ = load_bibtex(path)
 
-X_train, X_test, Y_train, Y_test = train_test_split(X, Y, test_size=0.33, random_state=42)
+#X_train, X_test, Y_train, Y_test = train_test_split(X, Y, test_size=0.33, random_state=42)
 
 
-n_tr = X_train.shape[0]
-n_te = X_test.shape[0]
-input_dim = X_train.shape[1]
-label_dim = Y_train.shape[1]
+#n_tr = X_train.shape[0]
+#n_te = X_test.shape[0]
+#input_dim = X_train.shape[1]
+#label_dim = Y_train.shape[1]
 
-print(f'Train set size = {n_tr}')
-print(f'Test set size = {n_te}')
-print(f'Input dim. = {input_dim}')
-print(f'Output dim. = {label_dim}')
-print(len(Y_test))
-print(len(Y_train))
+# print(f'Train set size = {n_tr}')
+# print(f'Test set size = {n_te}')
+# print(f'Input dim. = {input_dim}')
+# print(f'Output dim. = {label_dim}')
+# print(len(Y_test))
+# print(len(Y_train))
 
 
-lp = LineProfiler()
-lp_wrapper = lp(load_bibtex(dir_path))
-print(lp.print_stats())
