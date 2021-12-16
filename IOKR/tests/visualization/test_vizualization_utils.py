3c4718a20f2c4b28c5e34df3899ebda7f47534cb
DaSch17 <60350433+DaSch17@users.noreply.github.com>
2021-12-16 10:21:30 +0100

    Create test_vizualization_utils.py

---
+++ IOKR/tests/visualization/test_vizualization_utils.py
@@ -0,0 +1,11 @@
+import pytest
+import matplotlib.pyplot as plt
+from IOKR.visualization.visualization_utils import plot_results
+
+#class TestPlotResults():
+#
+#    @pytest.mark.mpl_image_compare
+#    def test_plot_results(self):
+#        params = ???
+#        return plot_results(str_score: str, train_loss: List, valid_loss: List,
+#                 valid_score: List, train_score: Optional[List] = None) -> None:
