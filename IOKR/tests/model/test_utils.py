fa0ff8f2d5c3caa5c1ebc05a9bebbd940a052943
DaSch17 <60350433+DaSch17@users.noreply.github.com>
2021-12-16 11:30:50 +0100

    Update test_utils.py

--- IOKR/tests/model/test_utils.py
+++ IOKR/tests/model/test_utils.py
@@ -12,11 +12,21 @@ class TestCreatePathThatDoesntExist():
         assert isdir(d1) is False, f'{d1} already exist'
         create_path_that_doesnt_exist(d1, "Check", "txt")
         assert isdir(d1) is True, f'{d1} has not been created'
+        
+class TestProjectRoot():
 
-#class TestProjectRoot():
-
-#    def test_project_root(self, tmp_path):
-#        d1 = tmp_path / 'd1'
-#        d2 = d1 / 'd2'
-#        d3 = d2 / 'd3'
-#        d4 = d3 / 'd4'
+    def test_project_root(self):
+        """Test for project_root function"""
+        #Get the actual path
+        actual_path = dirname(__file__)
+        #Get the supposed root_path to check
+        root_path = dirname(project_root())
+        #Get the difference in length of both
+        a = len(actual_path)
+        b = len(root_path)
+        dif_length = a - b
+        #Subtract the difference to the actual path
+        root_actual_path = actual_path[:-dif_length]
+        #Assert that the root path from the function is the same as the root of the actual path
+        assert root_actual_path == root_path, \
+            f'Root_path should be {root_path}, but instead returned {root_actual_path}'
