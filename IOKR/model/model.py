2fcde846837a4ad19e92c752e8105027d9cf7c9b
Gaetan Brison <gaetan.brison@gmail.com>
2022-03-15 10:32:56 +0100

    test

--- IOKR/model/model.py
+++ IOKR/model/model.py
@@ -19,6 +19,8 @@ from line_profiler import LineProfiler
 #dir_path = os.path.dirname(os.path.realpath(__file__))
 #dataset = arff.load(open('/Users/gaetanbrison/Documents/GitHub/IOKR/IOKR/data/bibtex/bibtex.arff'), "r")
 
+
+
 """
 Created on December 12, 2021
 """
